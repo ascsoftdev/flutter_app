38de72b5fb028a5e96d6d50a58a523fd66a6d138
achauhan <ascsoftdev@gmail.com>
2023-03-18 22:28:39 +0530

    Images, Font, Login Page without backend

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,12 +4,36 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(fontSize: 20, color: Colors.blue),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(children: [
+          Image.asset("assets/images/login_page.png", fit: BoxFit.cover),
+          const SizedBox(height: 20),
+          const Text("Welcome",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(),
+              child: const Text("Login"))
+        ]));
   }
 }
