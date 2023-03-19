dff5a4f1be136fd227fd4d0840bab726b6b418ad
achauhan <ascsoftdev@gmail.com>
2023-03-19 15:08:12 +0530

    Login Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Catalog App")),
-      body: Center(
-        child: Container(
-          child: const Text("Welcome to Hello Workd App"),
+        appBar: AppBar(title: const Text("Catalog App")),
+        body: Center(
+          child: Container(
+            child: const Text("Welcome to Hello Workd App"),
+          ),
         ),
-      ),
-      drawer: Drawer(),
-    );
+        drawer: Drawer());
   }
 }
